fef3e2859670bae9adc20dae1cec310667717144
TodorYordanov88 <tyordanov88@gmail.com>
2017-05-25 02:28:25 +0300

    Update

--- 04.BeverageLabels/BeverageLabels.cs
+++ 04.BeverageLabels/BeverageLabels.cs
@@ -10,6 +10,19 @@ namespace _04.BeverageLabels
     {
         static void Main(string[] args)
         {
+            string name = Console.ReadLine();
+            var volume = double.Parse(Console.ReadLine());
+            var energyContent = double.Parse(Console.ReadLine());
+            var sugarContent = double.Parse(Console.ReadLine());
+
+            var kCal = (volume / 100) * energyContent;
+            var sugar = (volume / 100) * sugarContent;
+
+            Console.WriteLine($"{volume}ml {name}:");
+            Console.WriteLine($"{kCal}kcal, {sugar}g sugars");
+
+
+
         }
     }
 }
--- 05.CharacterStats/CharacterStats.cs
+++ 05.CharacterStats/CharacterStats.cs
@@ -10,6 +10,18 @@ namespace _05.CharacterStats
     {
         static void Main(string[] args)
         {
+            String name = Console.ReadLine();
+            var currentHealth = int.Parse(Console.ReadLine());
+            var maximumHealth = int.Parse(Console.ReadLine());
+            var currentEnergy = int.Parse(Console.ReadLine());
+            var maximumEnergy = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}");
+            Console.Write($"Health: " + new string('|', 1) + new string('|', currentHealth ) + 
+                new string('.', maximumHealth - currentHealth) + new string('|', 1));
+            Console.WriteLine();
+            Console.Write($"Energy: " + new string('|', 1) + new string('|', currentEnergy) +
+                              new string('.', maximumEnergy - currentEnergy) + new string('|', 1));
         }
     }
 }
