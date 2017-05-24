2e3ef3eeb602f25f6049e291fba887ce6e2457f4
TodorYordanov88 <tyordanov88@gmail.com>
2017-05-24 23:51:57 +0300

    Adding Solutions

---
+++ 03.MilesToKilometers/MilesToKilometers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.MilesToKilometers
+{
+    class MilesToKilometers
+    {
+        static void Main(string[] args)
+        {
+            var mile = double.Parse(Console.ReadLine());
+            var mileToKilimoter = mile * 1.60934;
+            Console.WriteLine($"{mileToKilimoter:F2}");
+        }
+    }
+}
